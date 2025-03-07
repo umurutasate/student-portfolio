0d2cd9c01c2ad18aaa61d92bea2385f5b0b99703
umurutasate <129268927+umurutasate@users.noreply.github.com>
2025-03-07 09:48:24 +0200

    Update contact.py

--- contact.py
+++ contact.py
@@ -19,7 +19,7 @@ if page =="Home":
 	if uploaded_image is not None:
 		st.image(uploaded_image, width=150, caption="Uploaded image")
 	else:
-		st.image("pic.JPEG", width=150, caption="Default image")
+		st.image("pic.JPG", width=150, caption="Default image")
 	#Student details (Editable!)
 	name = st.text_input("Name: ", "cecile")
 	location = st.text_input("Location: ", "Musanze,Rwanda")
